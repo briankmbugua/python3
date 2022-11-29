9c3dd641786d9cee98ea56edf26d8c74ef9ec774
briankmbugua <briankmbuguak@gmail.com>
2022-11-29 13:06:03 +0300

    variable declaration

---
+++ variables.py
@@ -0,0 +1,11 @@
+x = 5
+y = "john"
+print(x)
+print(y)
+#variables do not need to be of any particular type, cna even change type
+#after being set
+
+b = 10 #b is of type int
+print(b)
+b = "brian" #b is now of type str
+print(b)
