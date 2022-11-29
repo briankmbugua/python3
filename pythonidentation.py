f7d94a15c95965341507e70afcabf8eba88ca21b
briankmbugua <briankmbuguak@gmail.com>
2022-11-29 13:04:37 +0300

    using identation for scope

---
+++ pythonidentation.py
@@ -0,0 +1,7 @@
+if 5 > 2:
+    print("Five is greator than two")
+    #  print("r") # this won't work, you have to use the same number of
+     #spaces in the same block
+    # python identation
+# if 4 > 2:
+# print("four is greator than two") # this will produce identation error        
