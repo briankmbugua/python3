186cfcdae7e277dcbe88ef79205021aefa51567e
briankmbugua <briankmbuguak@gmail.com>
2022-11-29 13:03:24 +0300

    using comments in  python

---
+++ comments.py
@@ -0,0 +1,10 @@
+print("comments in python")
+#this is a comment
+print("pyhton ignores string literals not assinged to a variable")
+print("you can use multiline string literals(tripple quotes)")
+print("to create multline comments")
+"""
+This is a comment
+written in
+more than just one line
+"""
