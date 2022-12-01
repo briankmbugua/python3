d02ae93ae39994d87f0423c3c88546795ff0d5ad
briankmbugua <briankmbuguak@gmail.com>
2022-12-01 23:57:17 +0300

    strings in pyhton

---
+++ strings.py
@@ -0,0 +1,25 @@
+x = "hello, world"
+
+print(x)
+
+##multline string using three quotes single or double
+multiline = """lorem ip sum.
+lorem ip dam,
+totem pi sum
+"""
+print(multiline)
+
+##looping through a string
+print("looping through a string")
+for x in "banan":
+  print(x)
+
+#string length
+name = "brian"
+len = len(name)
+print(len)
+
+#check string
+txt = "The best things in life are free!"
+
+print("free" in txt) ## True
